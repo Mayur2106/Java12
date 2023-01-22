016bf21fe41d99664dcd2aa4e669326cffff3891
Mayur <mayur2106wandile@gmail.com>
2023-01-22 17:04:23 +0530

    'added'

---
+++ src/pack1/day1.java
@@ -0,0 +1,26 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class day1 
+{
+
+	public static void main (String args[])
+	{  
+		
+		
+		System.out.println("welcome to office  ");
+		System.out.println("hello sir ");
+		
+		System.out.println("i am praful sequrity speaking");
+		System.out.println("Praful sir is out of office ");
+		
+		System.out.println("you can sit here  ");
+		System.out.println("unntil praful sir is not come to office");
+		System.out.println("By By sir ");
+		
+		
+		System.out.println("");
+	}
+	
+}
